b6835c40496775f023404fdbb95c6459a24d4e68
Pedro Rodrigues <pedroar2003@gmail.com>
2023-12-11 18:29:28 -0300

    Projeto criado

---
+++ src/main/java/com/zeush/project/Person.java
@@ -0,0 +1,21 @@
+package com.zeush.project;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+
+public class Person {
+
+	
+	private String name; 
+	
+	private int age;
+
+	
+}
