b9890fedbfff2aeaba791a9646662330e391f1a1
Pedro Rodrigues <pedroar2003@gmail.com>
2023-12-12 16:50:53 -0300

    métodos buscarTodos e buscarPeloId, camada de serviço, registro de componentes

--- src/main/java/com/zeush/admin/controllers/PacienteController.java
+++ src/main/java/com/zeush/admin/controllers/PacienteController.java
@@ -1,19 +1,32 @@
 package com.zeush.admin.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zeush.admin.entities.Paciente;
+import com.zeush.admin.services.PacienteService;
 
 @RestController
 @RequestMapping(value = "/pacientes")
 public class PacienteController {
 
+	@Autowired
+	private PacienteService service; 
+	
 	@GetMapping
-	public ResponseEntity<Paciente> buscarTodos() {
-		Paciente p = new Paciente(1L, "Pedro", "Pedro@gmail.com", "masc", "98078675", "07052003", 1);
-		return ResponseEntity.ok().body(p); 
+	public ResponseEntity<List<Paciente>> buscarTodos() {
+		List<Paciente> list = service.buscarTodos();
+		return ResponseEntity.ok().body(list); 
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Paciente> buscarPorId(@PathVariable Long id) {
+		Paciente obj = service.buscarPorId(id);
+		return ResponseEntity.ok().body(obj); 
 	}
 }
---
+++ src/main/java/com/zeush/admin/services/PacienteService.java
@@ -0,0 +1,28 @@
+package com.zeush.admin.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zeush.admin.entities.Paciente;
+import com.zeush.admin.repositories.PacienteRepository;
+
+@Service
+public class PacienteService {
+
+	@Autowired
+	private PacienteRepository repository; 
+	
+	public List<Paciente> buscarTodos() {
+		return repository.findAll(); 
+	}
+	
+	public Paciente buscarPorId(Long id) {
+		Optional<Paciente> obj = repository.findById(id);
+		return obj.get(); 
+	}
+	
+	
+}
